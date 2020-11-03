5064657f89098f34ed9d149bb3d3ada9618ead7a
Andrei Roibu <aroibu@itairbook1.local>
2020-11-03 15:47:08 +0000

    created setup files for environments

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UnsupervisedDeepLearning',
+    version='0.0.1',
+    description='Unsupervised Deep Learning Course Material',
+    license='BSD 3-clause license',
+    maintainer='Andrei-Claudiu Roibu',
+    maintainer_email='andrei-claudiu.roibu@dtc.ox.ac.uk',
+    install_requires=[
+        'pip',
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'sklearn',
+        ],
+)
